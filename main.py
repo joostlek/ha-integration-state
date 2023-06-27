90a6a12c80cfef54e0d565e29388a3ed82bcc958
joostlek <joostlek@outlook.com>
2023-06-27 14:24:35 +0200

    Initial test

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import subprocess
 import re
 
 def is_real_integration(name: str) -> bool:
-    return os.path.isfile(f"homeassistant/components/{name}/__init__.py")
-integrations = [x for x in os.listdir("homeassistant/components") if is_real_integration(x)]
+    return os.path.isfile(f"core/homeassistant/components/{name}/__init__.py")
+integrations = [x for x in os.listdir("core/homeassistant/components") if is_real_integration(x)]
 
 df = pd.DataFrame(index=integrations, columns=["quality_scale", "config_flow", "sensor", "iot_class", "integration_type", "external_library", "codeowners", "has_tests", "coverage"])
 def has_feature(name: str, featur: str) -> bool:
-    return os.path.isfile(f"homeassistant/components/{name}/{featur}.py")
+    return os.path.isfile(f"core/homeassistant/components/{name}/{featur}.py")
 df = df.sort_index()
 for integration_name, integration in df.iterrows():
     for feature in ("config_flow", "sensor"):
         df.at[integration_name, feature] = has_feature(integration_name, feature)
-    with open(f"homeassistant/components/{integration_name}/manifest.json") as manifest_file:
+    with open(f"core/homeassistant/components/{integration_name}/manifest.json") as manifest_file:
         manifest = json.load(manifest_file)
     if "integration_type" in manifest:
         df.at[integration_name, "integration_type"] = manifest["integration_type"]
@@ -29,16 +29,16 @@ for integration_name, integration in df.iterrows():
     df.at[integration_name, "codeowners"] = 0
     if "codeowners" in manifest:
         df.at[integration_name, "codeowners"] = len(manifest["codeowners"])
-    has_tests = os.path.exists(f"tests/components/{integration_name}")
+    has_tests = os.path.exists(f"core/tests/components/{integration_name}")
     df.at[integration_name, "has_tests"] = has_tests
-    if has_tests:
-        try:
-            p = subprocess.Popen(["pytest", f"tests/components/{integration_name}", f"--cov=homeassistant.components.{integration_name}"], stdout=subprocess.PIPE)
-            test, err = p.communicate()
-            regex = r"TOTAL.*\s(\d*)%"
-            result = re.search(regex, str(test))
-            df.at[integration_name, "coverage"] = result.group(1)
-        except:
-            pass
+    # if has_tests:
+    #     try:
+    #         p = subprocess.Popen(["pytest", f"core/tests/components/{integration_name}", f"--cov=homeassistant.components.{integration_name}"], stdout=subprocess.PIPE)
+    #         test, err = p.communicate()
+    #         regex = r"TOTAL.*\s(\d*)%"
+    #         result = re.search(regex, str(test))
+    #         df.at[integration_name, "coverage"] = result.group(1)
+    #     except:
+    #         pass
 
 df.to_excel("./state.xlsx")
