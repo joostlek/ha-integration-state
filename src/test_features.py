3570b6c46600040d66d86a5d0af85973f0294c8e
joostlek <joostlek@outlook.com>
2023-06-28 16:41:55 +0200

    Add more entity platforms

--- src/test_features.py
+++ src/test_features.py
@@ -5,7 +5,7 @@ from const import ENTITY_PLATFORMS
 df = pd.read_csv("output/base.csv", index_col=0)
 
 for entity_platform in ENTITY_PLATFORMS:
-    df[f"Tests: {entity_platform}"] = False
+    df[f"Tests: {entity_platform}"] = None
 
 for integration_id, integration in df.iterrows():
     domain = integration["domain"]
