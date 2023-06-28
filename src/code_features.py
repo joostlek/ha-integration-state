cd3aeca1ba7aaa8c4792199d00ede41f2e2e6a64
joostlek <joostlek@outlook.com>
2023-06-28 16:37:42 +0200

    Add more entity platforms

--- src/code_features.py
+++ src/code_features.py
@@ -1,20 +1,16 @@
 import os
 import pandas as pd
 
+from src.const import ENTITY_PLATFORMS
+
 df = pd.read_csv("output/base.csv", index_col=0)
 
-df["sensor"] = False
-df["coordinator"] = False
-df["binary_sensor"] = False
-df["config_flow"] = False
-df["diagnostics"] = False
-df["light"] = False
-df["scene"] = False
-df["switch"] = False
+for entity_platform in ENTITY_PLATFORMS:
+    df[entity_platform] = False
 
 for integration_id, integration in df.iterrows():
     domain = integration["domain"]
-    for feature in ("config_flow", "sensor", "coordinator", "binary_sensor", "diagnostics", "light", "scene", "switch"):
+    for feature in ENTITY_PLATFORMS:
         df.at[integration_id, feature] = os.path.isfile(f"core/homeassistant/components/{domain}/{feature}.py")
 
 
--- src/test_features.py
+++ src/test_features.py
@@ -1,21 +1,17 @@
 import os
 import pandas as pd
+from src.const import ENTITY_PLATFORMS
 
 df = pd.read_csv("output/base.csv", index_col=0)
 
-df["test_sensor"] = None
-df["test_binary_sensor"] = None
-df["test_config_flow"] = None
-df["test_diagnostics"] = None
-df["test_light"] = None
-df["test_scene"] = None
-df["test_switch"] = None
+for entity_platform in ENTITY_PLATFORMS:
+    df[f"Tests: {entity_platform}"] = False
 
 for integration_id, integration in df.iterrows():
     domain = integration["domain"]
-    for feature in ("config_flow", "sensor", "binary_sensor", "diagnostics", "light", "scene", "switch"):
+    for feature in ENTITY_PLATFORMS:
         if os.path.isfile(f"core/homeassistant/components/{domain}/{feature}.py"):
-            df.at[integration_id, f"test_{feature}"] = os.path.isfile(f"core/tests/components/{domain}/test_{feature}.py")
+            df.at[integration_id, f"Tests: {feature}"] = os.path.isfile(f"core/tests/components/{domain}/test_{feature}.py")
 
 
 df.to_csv("output/test_features.csv")
