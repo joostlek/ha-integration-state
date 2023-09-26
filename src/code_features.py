5c1ae393ac331123ff54fb00ea367eeb6823cdcf
Joostlek <joostlek@outlook.com>
2023-09-26 18:36:13 +0200

    Track config flow

--- src/code_features.py
+++ src/code_features.py
@@ -4,13 +4,13 @@ import pandas as pd
 from const import ENTITY_PLATFORMS
 
 df = pd.read_csv("output/base.csv", index_col=0)
-
-for entity_platform in ENTITY_PLATFORMS:
+files = [*ENTITY_PLATFORMS, "config_flow"]
+for entity_platform in files:
     df[entity_platform] = False
 
 for integration_id, integration in df.iterrows():
     domain = integration["domain"]
-    for feature in ENTITY_PLATFORMS:
+    for feature in files:
         df.at[integration_id, feature] = os.path.isfile(f"core/homeassistant/components/{domain}/{feature}.py")
 
 
